3c909d2fe7a957afdc612e29738841ceadd30fab
Kwakcena <rhkrgudwh@gmail.com>
2020-04-17 23:49:41 +0900

    여러 숫자의 버튼 추가
    map을 활용해서 버튼을 여러개 추가함.
    props에 함수로 매개변수를 전달할 때는 () => onIncrease(number) 처럼

--- counter-app/src/components/Counter.js
+++ counter-app/src/components/Counter.js
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
 
 const Counter = () => {
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   const [number, setNumber] = useState(0);
-  const onIncrease = () => setNumber((number) => number + 1);
-  const onDecrease = () => setNumber((number) => number - 1);
+
+  const onIncrease = (number) => setNumber((num) => num + number);
+  const onDecrease = (number) => setNumber((num) => num - number);
 
   return (
     <div>
       <h2>number : {number}</h2>
       <hr />
-      <button onClick={onIncrease}>증가 +1</button>
-      <button onClick={onDecrease}>감소 -1</button>
+      증가 버튼 :
+      {numbers.map((number, index) => (
+        <button key={index} onClick={() => onIncrease(number)}>
+          +{number}
+        </button>
+      ))}
+      <br />
+      감소 버튼 :
+      {numbers.map((number, index) => (
+        <button key={index} onClick={() => onDecrease(number)}>
+          -{number}
+        </button>
+      ))}
     </div>
   );
 };
